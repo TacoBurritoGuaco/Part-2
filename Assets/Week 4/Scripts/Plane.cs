855b342c83b352745a038868893fe54e6ad33432
Fernando Salas Duran <salasdur@shernet.sheridancollege.ca>
2024-02-01 14:25:09 -0500

    Moving the plane Updating flight path

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -10,12 +10,45 @@ public class Plane : MonoBehaviour
     public float newPointTreshold = 0.2f;
     Vector2 lastPosition;
     LineRenderer lineRenderer;
+    Rigidbody2D rigidbody;
+    Vector2 currentPos;
+    public float speed = 1;
 
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
+
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        currentPos = transform.position;
+        if (points.Count > 0)
+        {
+            Vector2 direction = points[0] - currentPos;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg; //Important math commands!
+            rigidbody.rotation = -angle; //has to be negative!
+        }
+        rigidbody.MovePosition(rigidbody.position + (Vector2)transform.up*speed*Time.deltaTime);
+    }
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        if (points.Count > 0)
+        {
+           if(Vector2.Distance(currentPos, points[0]) < newPointTreshold) 
+           { 
+                points.RemoveAt(0);
+                for(int i = 0; i < lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i+1));
+                }
+                lineRenderer.positionCount--;
+           }
+        }
     }
 
     private void OnMouseDown()
