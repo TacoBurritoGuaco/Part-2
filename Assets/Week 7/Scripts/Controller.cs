10ad38f12d481f9377aede8eff604993a6eba31b
Fernando Salas Duran <salasdur@shernet.sheridancollege.ca>
2024-02-22 15:22:12 -0500

    Task2-A completed Players can now be individually selected and de-selected This is done using static! its really handy!
    Task2-B Completed as well (I forgot to commit the last thing...)
    Player moves towards the mouse using force
    This is done using, once more, the controller!

---
+++ Assets/Week 7/Scripts/Controller.cs
@@ -0,0 +1,61 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Slider chargeSlider;
+    float charge;
+    public float maxCharge = 1;
+    Vector2 direction;
+
+    public static FootballPlayer SelectedPlayer { get; private set; } //variable that holds the currently selected player
+    //Belongs to the class controller, rather than an object using this
+    //Anyone can access this, but only THIS can set its value
+    //Get: Determines whether or not you can get this static variable
+    //Set: Determines whether or not you can set this static variable
+
+    //As shown below, you can do this with functions as well! saves a lot of time!
+    public static void SetSelectedPlayer(FootballPlayer p)
+    {
+        if(SelectedPlayer != null) //if there IS a selected player
+        {
+            SelectedPlayer.Selected(false); //unselect yourself
+        }
+        //Set the "selected player" to the inputted player
+        SelectedPlayer = p;
+        //Set the selected player to true for the new inputted player
+        SelectedPlayer.Selected(true);
+    }
+    private void FixedUpdate()
+    {
+        if (direction != Vector2.zero)
+        {
+            SelectedPlayer.Move(direction);
+            direction = Vector2.zero;
+            charge = 0;
+            chargeSlider.value = charge;
+        }
+    }
+    private void Update()
+    {
+        if (SelectedPlayer == null) return;
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            charge = 0;
+            direction = Vector2.zero;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            charge += Time.deltaTime;
+            charge = Mathf.Clamp(charge, 0, maxCharge);
+            chargeSlider.value = charge;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            direction = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)SelectedPlayer.transform.position).normalized * charge;
+        }
+    }
+}
--- Assets/Week 7/Scripts/FootballPlayer.cs
+++ Assets/Week 7/Scripts/FootballPlayer.cs
@@ -5,20 +5,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 
 public class FootballPlayer : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Color oldColor;
+    Rigidbody2D rb;
+    public float speed = 500;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        oldColor = spriteRenderer.color;
         Selected(false);
     }
 
     private void OnMouseDown()
     {
-        Selected(true);
+        Controller.SetSelectedPlayer(this);
     }
     public void Selected(bool isSelected)
     {
@@ -27,7 +32,12 @@ public class FootballPlayer : MonoBehaviour
             spriteRenderer.color = Color.yellow;
         }
         else {
-            oldColor = spriteRenderer.color;
+            spriteRenderer.color = oldColor;
         }
+    }
+
+    public void Move(Vector2 direction)
+    {
+        rb.AddForce(direction * speed);
     }
 }
