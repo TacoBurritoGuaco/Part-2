4a3019d45e7269aabcf2ed303ec6350e59ff443a
Fernando Salas Duran <salasdur@shernet.sheridancollege.ca>
2024-02-28 18:33:10 -0500

    Going to be real, this is going awful For the life of me I cannot figure out how to get an object to simply just point towards a random point on the screen I am going to scrap the following: Knife Halberd and Sword all of which relied on the above specifically so I can hand this in on time I will also be scrapping the complex object spawning method in favour of a significantly simpler one given the monumental size-down of the project Knife.cs will remain specifically if I ever want to revisit this

--- Assets/Assignment/Scripts/Weapons/Knife.cs
+++ Assets/Assignment/Scripts/Weapons/Knife.cs
@@ -12,13 +12,14 @@ public class Knife : WeaponBase
     {
         base.Start(); //using the rest of the base function's base
         movement = new Vector2 (Random.Range(-1, 2), Random.Range(-1, 2)); //a random position that the knife will move towards
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(movement.y, movement.x + 180)); //quaternion to make sure rotation is not messy.
+        //The rotation of the knife is determined by atan, which finds the angle of movement in radians
+        //Afterwards, it is turned into degrees.
+        rb.MoveRotation(Mathf.Rad2Deg * Mathf.Atan2(movement.y, movement.x) + 180); //rigidbody rotation
     }
     //Fixed update moves the knife towards a specified point off-screen
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime); //Moves towards a specified point in an specified direction
-        transform.rotation = Quaternion.Euler(0, 0, Vector2.Angle(transform.position.normalized, movement.normalized)); //quaternion to make sure rotation is not messy (specifically transform rotation)
-        rb.MoveRotation(Vector2.Angle(transform.position.normalized, movement.normalized)); //rigidbody rotation done using vector2.angle
-        //This function returns the angle between two different vectors, effectively giving us how much the knife should rotate
     }
 }
